5e56df3f097d4d25eeda1a52edcf90dbdea256fe
Michal Witkowski <michal@improbable.io>
2016-12-16 14:53:12 +0000

    added metadata helpers

---
+++ util/metautils/doc.go
@@ -0,0 +1,13 @@
+// Copyright 2016 Michal Witkowski. All Rights Reserved.
+// See LICENSE for licensing terms.
+
+/*
+Package `metautils` provides convenience functions for dealing with gRPC metadata.MD objects inside
+Context handlers.
+
+While the upstream grpc-go package contains decent functionality (see https://github.com/grpc/grpc-go/blob/master/Documentation/grpc-metadata.md)
+they are hard to use.
+*/
+
+
+package metautils
---
+++ util/metautils/single_key.go
@@ -0,0 +1,60 @@
+// Copyright 2016 Michal Witkowski. All Rights Reserved.
+// See LICENSE for licensing terms.
+
+package metautils
+
+import (
+	"encoding/base64"
+	"strings"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/metadata"
+)
+
+const (
+	binHdrSuffix = "-bin"
+)
+
+// GetSingle extracts a single-value metadata key from Context.
+// First return is the value of the key, followed by a bool indicator.
+// The bool indicator being false means the string should be discarded. It can be false if
+// the context has no metadata at all, the key in metadata doesn't exist or there are multiple values.
+func GetSingle(ctx context.Context, keyName string) (string, bool) {
+	// TODO(mwitkow): Fix binary content support.
+	md, ok := metadata.FromContext(ctx)
+	if !ok {
+		return "", false
+	}
+	keyName = strings.ToLower(keyName)
+	valSlice, ok := md[keyName]
+	if !ok {
+		return "", false
+	}
+	if len(valSlice) != 1 {
+		return "", false
+	}
+	return valSlice[0], true
+}
+
+// SetSingle sets or overrides a metadata key to be single value in the Context.
+// It returns a new context.Context object that contains a *copy* of the metadata inside the given
+// context.
+func SetSingle(ctx context.Context, keyName string, keyValue string) context.Context {
+	md, ok := metadata.FromContext(ctx)
+	if !ok {
+		md = metadata.Pairs(keyName, keyValue)
+		return metadata.NewContext(ctx, md)
+	}
+	k, v := encodeKeyValue(keyName, keyValue)
+	md[k] = []string{v}
+	return ctx // we use the same context because we modified the metadata in place.
+}
+
+func encodeKeyValue(k, v string) (string, string) {
+	k = strings.ToLower(k)
+	if strings.HasSuffix(k, binHdrSuffix) {
+		val := base64.StdEncoding.EncodeToString([]byte(v))
+		v = string(val)
+	}
+	return k, v
+}
