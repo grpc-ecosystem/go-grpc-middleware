daa67ba04005579db1b3696631a89bed88d04c12
Michal Witkowski <michal@improbable.io>
2016-12-18 09:18:44 +0100

    add support for tls in test suites

--- testing/interceptor_suite.go
+++ testing/interceptor_suite.go
@@ -7,26 +7,34 @@ import (
 	"net"
 	"time"
 
+	"flag"
+	"path"
+	"runtime"
+
 	pb_testproto "github.com/mwitkow/go-grpc-middleware/testing/testproto"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"flag"
+	"google.golang.org/grpc/credentials"
 )
 
 var (
-	flagTls = flag.Bool("use_tls", false, "whether all gRPC middleware tests should use tls")
+	flagTls = flag.Bool("use_tls", true, "whether all gRPC middleware tests should use tls")
 )
 
+func getTestingCertsPath() string {
+	_, callerPath, _, _ := runtime.Caller(0)
+	return path.Join(path.Dir(callerPath), "certs")
+}
 
 // InterceptorTestSuite is a testify/Suite that starts a gRPC PingService server and a client.
 type InterceptorTestSuite struct {
 	suite.Suite
 
 	TestService pb_testproto.TestServiceServer
-	ServerOpts []grpc.ServerOption
-	ClientOpts []grpc.DialOption
+	ServerOpts  []grpc.ServerOption
+	ClientOpts  []grpc.DialOption
 
 	ServerListener net.Listener
 	Server         *grpc.Server
@@ -38,7 +46,14 @@ func (s *InterceptorTestSuite) SetupSuite() {
 	var err error
 	s.ServerListener, err = net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(s.T(), err, "must be able to allocate a port for serverListener")
-
+	if *flagTls {
+		creds, err := credentials.NewServerTLSFromFile(
+			path.Join(getTestingCertsPath(), "localhost.crt"),
+			path.Join(getTestingCertsPath(), "localhost.key"),
+		)
+		require.NoError(s.T(), err, "failed reading server credentials for localhost.crt")
+		s.ServerOpts = append(s.ServerOpts, grpc.Creds(creds))
+	}
 	// This is the point where we hook up the interceptor
 	s.Server = grpc.NewServer(s.ServerOpts...)
 	// Crete a service of the instantiator hasn't provided one.
@@ -54,7 +69,15 @@ func (s *InterceptorTestSuite) SetupSuite() {
 }
 
 func (s *InterceptorTestSuite) NewClient(dialOpts ...grpc.DialOption) pb_testproto.TestServiceClient {
-	newDialOpts := append(dialOpts, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(2*time.Second))
+	newDialOpts := append(dialOpts, grpc.WithBlock(), grpc.WithTimeout(2*time.Second))
+	if *flagTls {
+		creds, err := credentials.NewClientTLSFromFile(
+			path.Join(getTestingCertsPath(), "localhost.crt"), "localhost")
+		require.NoError(s.T(), err, "failed reading client credentials for localhost.crt")
+		newDialOpts = append(newDialOpts, grpc.WithTransportCredentials(creds))
+	} else {
+		newDialOpts = append(newDialOpts, grpc.WithInsecure())
+	}
 	clientConn, err := grpc.Dial(s.ServerAddr(), newDialOpts...)
 	require.NoError(s.T(), err, "must not error on client Dial")
 	return pb_testproto.NewTestServiceClient(clientConn)
@@ -65,7 +88,7 @@ func (s *InterceptorTestSuite) ServerAddr() string {
 }
 
 func (s *InterceptorTestSuite) SimpleCtx() context.Context {
-	ctx, _ := context.WithTimeout(context.TODO(), 2 * time.Second)
+	ctx, _ := context.WithTimeout(context.TODO(), 2*time.Second)
 	return ctx
 }
 
