19939be52938713a1e159fad01098630ec0d0f3b
Michal Witkowski <michal@improbable.io>
2017-04-05 10:46:12 +0100

    clarify extract semantics

--- tags/doc.go
+++ tags/doc.go
@@ -9,6 +9,9 @@ Request Context Tags
 Tags describe information about the request, and can be set and used by other middleware. Tags are used for logging
 and tracing of requests.
 
+If a user doesn't use the interceptors that initialize the `Tags` object, all operations following from an `Extract(ctx)`
+will be no-ops. This is to ensure that code doesn't panic if the interceptors weren't used.
+
 Tags fields are typed, and shallow and should follow the OpenTracing semantics convention:
 https://github.com/opentracing/specification/blob/master/semantic_conventions.md
 
