d8b0a7ecd2d44e0f25855f8a73dd9c409e75ef24
Eric Feliksik <feliksik@gmail.com>
2017-05-01 16:09:22 +0200

    fix backoff bug, the range should reach up and down, fully

--- util/backoffutils/backoff.go
+++ util/backoffutils/backoff.go
@@ -18,6 +18,6 @@ import (
 // This adds or substracts time from the duration within a given jitter fraction.
 // For example for 10s and jitter 0.1, it will returna  time within [9s, 11s])
 func JitterUp(duration time.Duration, jitter float64) time.Duration {
-	multiplier := jitter * (rand.Float64() - 0.5)
+	multiplier := jitter * (rand.Float64()*2 - 1)
 	return time.Duration(float64(duration) * (1 + multiplier))
 }
