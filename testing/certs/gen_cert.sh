645b33ed7ba8739747bf2df55d0349d4bba2e7f6
Michal Witkowski <michal@improbable.io>
2017-08-25 08:58:17 +0100

    Fixup certs that expired

--- testing/certs/gen_cert.sh
+++ testing/certs/gen_cert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Regenerate the self-signed certificate for local host.
 
-openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -keyout localhost.key -out localhost.crt
+openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 3650 -keyout localhost.key -out localhost.crt
 
