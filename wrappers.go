e92baf37067d7d73e864f972afff691091e6217b
Chris Duncan <veqryn@hotmail.com>
2018-08-25 03:44:55 -0600

    Use an interface for the wrapped server stream

--- wrappers.go
+++ wrappers.go
@@ -8,6 +8,12 @@ import (
 	"google.golang.org/grpc"
 )
 
+// WrappedContextSettingServerStream allows setting the wrapped context of a grpc.ServerStream.
+type WrappedContextSettingServerStream interface {
+	grpc.ServerStream
+	SetContext(context.Context)
+}
+
 // WrappedServerStream is a thin wrapper around grpc.ServerStream that allows modifying context.
 type WrappedServerStream struct {
 	grpc.ServerStream
@@ -20,9 +26,14 @@ func (w *WrappedServerStream) Context() context.Context {
 	return w.WrappedContext
 }
 
+// SetContext sets the wrapper's WrappedContext, overwriting the nested grpc.ServerStream.Context()
+func (w *WrappedServerStream) SetContext(ctx context.Context) {
+	w.WrappedContext = ctx
+}
+
 // WrapServerStream returns a ServerStream that has the ability to overwrite context.
-func WrapServerStream(stream grpc.ServerStream) *WrappedServerStream {
-	if existing, ok := stream.(*WrappedServerStream); ok {
+func WrapServerStream(stream grpc.ServerStream) WrappedContextSettingServerStream {
+	if existing, ok := stream.(WrappedContextSettingServerStream); ok {
 		return existing
 	}
 	return &WrappedServerStream{ServerStream: stream, WrappedContext: stream.Context()}
