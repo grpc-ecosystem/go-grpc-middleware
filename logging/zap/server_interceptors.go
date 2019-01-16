9b773408ad345798dfcd89a0e3b9476acefff39a
zxfishhack <zxfishhack@126.com>
2019-01-16 22:24:06 +0800

    fixed duplicate custom&peer.address field

--- logging/zap/server_interceptors.go
+++ logging/zap/server_interceptors.go
@@ -86,7 +86,7 @@ func serverCallFields(fullMethodString string) []zapcore.Field {
 }
 
 func newLoggerForCall(ctx context.Context, logger *zap.Logger, fullMethodString string, start time.Time) context.Context {
-	f := ctxzap.TagsToFields(ctx)
+	var f []zapcore.Field
 	f = append(f, zap.String("grpc.start_time", start.Format(time.RFC3339)))
 	if d, ok := ctx.Deadline(); ok {
 		f = append(f, zap.String("grpc.request.deadline", d.Format(time.RFC3339)))
