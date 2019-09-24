86774d7a2a91b40a2ee0d2a7194117bcbf982a45
Dominic Green <dominicgreen1@gmail.com>
2019-09-24 10:21:26 +0100

    simplify by using embedding

--- logging/logrus/grpclogger.go
+++ logging/logrus/grpclogger.go
@@ -4,8 +4,6 @@
 package grpc_logrus
 
 import (
-	"fmt"
-
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc/grpclog"
 )
@@ -13,61 +11,17 @@ import (
 // ReplaceGrpcLogger sets the given logrus.Logger as a gRPC-level logger.
 // This should be called *before* any other initialization, preferably from init() functions.
 func ReplaceGrpcLogger(logger *logrus.Entry) {
-	grpclog.SetLoggerV2(&logrusGrpcLoggerV2{logger: logger.WithField("system", SystemField)})
+	x := &logrusGrpcLoggerV2{
+		logger.WithField("system", SystemField),
+	}
+
+	grpclog.SetLoggerV2(x)
 }
 
 type logrusGrpcLoggerV2 struct {
-	logger *logrus.Entry
-}
-
-func (l *logrusGrpcLoggerV2) Info(args ...interface{}) {
-	l.logger.Info(args...)
-}
-
-func (l *logrusGrpcLoggerV2) Infoln(args ...interface{}) {
-	l.logger.Info(args...)
-}
-
-func (l *logrusGrpcLoggerV2) Infof(format string, args ...interface{}) {
-	l.logger.Info(fmt.Sprintf(format, args...))
-}
-
-func (l *logrusGrpcLoggerV2) Warning(args ...interface{}) {
-	l.logger.Warn(args...)
-}
-
-func (l *logrusGrpcLoggerV2) Warningln(args ...interface{}) {
-	l.logger.Warn(fmt.Sprint(args...))
-}
-
-func (l *logrusGrpcLoggerV2) Warningf(format string, args ...interface{}) {
-	l.logger.Warn(fmt.Sprintf(format, args...))
-}
-
-func (l *logrusGrpcLoggerV2) Error(args ...interface{}) {
-	l.logger.Error(args...)
-}
-
-func (l *logrusGrpcLoggerV2) Errorln(args ...interface{}) {
-	l.logger.Error(fmt.Sprint(args...))
-}
-
-func (l *logrusGrpcLoggerV2) Errorf(format string, args ...interface{}) {
-	l.logger.Error(fmt.Sprintf(format, args...))
-}
-
-func (l *logrusGrpcLoggerV2) Fatal(args ...interface{}) {
-	l.logger.Fatal(args...)
-}
-
-func (l *logrusGrpcLoggerV2) Fatalln(args ...interface{}) {
-	l.logger.Fatal(fmt.Sprint(args...))
-}
-
-func (l *logrusGrpcLoggerV2) Fatalf(format string, args ...interface{}) {
-	l.logger.Fatal(fmt.Sprintf(format, args...))
+	*logrus.Entry
 }
 
 func (l *logrusGrpcLoggerV2) V(level int) bool {
-	return int(l.logger.Level) >= level
+	return int(l.Level) >= level
 }
