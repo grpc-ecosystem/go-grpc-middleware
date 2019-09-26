9e989b054a77fcf636b96ea3dda07bd9247c0249
Peter van den Broek <peter.vandenbroek@isaac.nl>
2019-09-26 10:35:08 +0200

    Added a new JsonPbMarshal interface, used by the payload interceptors.
    This allows overriding this functionality with something the user provides.

--- logging/zap/payload_interceptors.go
+++ logging/zap/payload_interceptors.go
@@ -16,7 +16,7 @@ import (
 
 var (
 	// JsonPbMarshaller is the marshaller used for serializing protobuf messages.
-	JsonPbMarshaller = &jsonpb.Marshaler{}
+	JsonPbMarshaller grpc_logging.JsonPbMarshaler = &jsonpb.Marshaler{}
 )
 
 // PayloadUnaryServerInterceptor returns a new unary server interceptors that logs the payloads of requests.
