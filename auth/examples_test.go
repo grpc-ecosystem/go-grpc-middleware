1cc8c399b53bdb6e88b3a5cbb575fccd669e6f83
atlantr <t.esseling@protonmail.com>
2020-05-12 13:53:30 +0100

    resolved comments

--- auth/examples_test.go
+++ auth/examples_test.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"log"
 
-	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
-	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
-
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 	"google.golang.org/grpc/status"
+
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 )
 
 func parseToken(token string) (struct{}, error) {
@@ -21,7 +21,7 @@ func userClaimFromToken(struct{}) string {
 	return "foobar"
 }
 
-//exampleAuthFunc is used by a middleware to authenticate requests
+// exampleAuthFunc is used by a middleware to authenticate requests
 func exampleAuthFunc(ctx context.Context) (context.Context, error) {
 	token, err := grpc_auth.AuthFromMD(ctx, "bearer")
 	if err != nil {
@@ -35,7 +35,7 @@ func exampleAuthFunc(ctx context.Context) (context.Context, error) {
 
 	grpc_ctxtags.Extract(ctx).Set("auth.sub", userClaimFromToken(tokenInfo))
 
-	//WARNING: in production define your own type to avoid context collisions
+	// WARNING: in production define your own type to avoid context collisions
 	newCtx := context.WithValue(ctx, "tokenInfo", tokenInfo)
 
 	return newCtx, nil
@@ -51,19 +51,19 @@ func Example_serverConfig() {
 
 type gRPCserverAuthenticated struct{}
 
-//SayHello only can be called by client when authenticated by exampleAuthFunc
+// SayHello only can be called by client when authenticated by exampleAuthFunc
 func (g gRPCserverAuthenticated) SayHello(ctx context.Context, request *pb.HelloRequest) (*pb.HelloReply, error) {
 	return &pb.HelloReply{Message: "pong authenticated"}, nil
 }
 
 type gRPCserverUnauthenticated struct{}
 
-//SayHello can be called by client without being authenticated by exampleAuthFunc as AuthFuncOverride is called instead
+// SayHello can be called by client without being authenticated by exampleAuthFunc as AuthFuncOverride is called instead
 func (g *gRPCserverUnauthenticated) SayHello(ctx context.Context, request *pb.HelloRequest) (*pb.HelloReply, error) {
 	return &pb.HelloReply{Message: "pong unauthenticated"}, nil
 }
 
-//AuthFuncOverride is called instead of exampleAuthFunc
+// AuthFuncOverride is called instead of exampleAuthFunc
 func (g *gRPCserverUnauthenticated) AuthFuncOverride(ctx context.Context, fullMethodName string) (context.Context, error) {
 	log.Println("client is calling method:", fullMethodName)
 	return ctx, nil
@@ -76,8 +76,11 @@ func Example_serverConfigWithAuthOverride() {
 		grpc.UnaryInterceptor(grpc_auth.UnaryServerInterceptor(exampleAuthFunc)),
 	)
 
-	//in this case a single service is used,
-	//in most use cased there would be at least two services defined in the proto file
-	pb.RegisterGreeterServer(server, &gRPCserverAuthenticated{})
-	pb.RegisterGreeterServer(server, &gRPCserverUnauthenticated{})
+	overrideActive := true
+
+	if overrideActive {
+		pb.RegisterGreeterServer(server, &gRPCserverUnauthenticated{})
+	} else {
+		pb.RegisterGreeterServer(server, &gRPCserverAuthenticated{})
+	}
 }
