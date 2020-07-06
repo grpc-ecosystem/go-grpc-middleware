48d0d1381dfc006fa9ddac6c7aee8f2af4e495df
Yash Sharma <yashrsharma44@gmail.com>
2020-07-06 11:55:59 +0530

    added pre call implementation
    Signed-off-by: Yash Sharma <yashrsharma44@gmail.com>

--- interceptors/logging/interceptors.go
+++ interceptors/logging/interceptors.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors"
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/tags"
@@ -23,14 +22,13 @@ func extractFields(tags tags.Tags) Fields {
 }
 
 type reporter struct {
-	ctx             context.Context
-	typ             interceptors.GRPCType
-	service, method string
-
-	requestLoggerDecision bool
-
-	opts   *options
-	logger Logger
+	ctx                context.Context
+	typ                interceptors.GRPCType
+	service, method    string
+	firstMessageLogged bool
+	isServer           bool
+	opts               *options
+	logger             Logger
 
 	kind string
 }
@@ -54,26 +52,54 @@ func (c *reporter) PostCall(err error, duration time.Duration) {
 	logger.With(c.opts.durationFieldFunc(duration)...).Log(c.opts.levelFunc(code), fmt.Sprintf("finished %s %s call", c.kind, c.typ))
 }
 
-// PostMsgSend logs the details of the servingObject(req/resp) that is flowing out of the rpc.
-func (c *reporter) PostMsgSend(req interface{}, err error, duration time.Duration) {
-	// If the Request Logging is configured to be skipped.
-	if !c.requestLoggerDecision {
+// PostMsgSend logs the details of the servingObject that is flowing out of the rpc.
+// resp object wrt server.
+func (c *reporter) PostMsgSend(resp interface{}, err error, duration time.Duration) {
+	if !c.opts.shouldLog(interceptors.FullMethod(c.service, c.method), err) {
 		return
 	}
-	code := c.opts.codeFunc(err)
+	// If the first message is logged skip the rest of the logging.
+	if c.firstMessageLogged {
+		return
+	}
+	c.firstMessageLogged = true
+
 	logger := c.logger.With(extractFields(tags.Extract(c.ctx))...)
-	logRequestResponse(c.ctx, logger.With("grpc.recv.duration", duration.String()), req, c.opts.levelFunc(code))
+	logger = logger.With("grpc.recv.duration", duration.String())
+	if err != nil {
+		logger = logger.With("error", fmt.Sprintf("%v", err))
+	}
+
+	if c.isServer {
+		logger.Log("logged the start of the response.", fmt.Sprintf("response object: %v", resp))
+	} else {
+		logger.Log("logged the start of the request.", fmt.Sprintf("request object: %v", resp))
+	}
 }
 
 // PostMsgReceive logs the details of the servingObject that is flowing into the rpc.
-func (c *reporter) PostMsgReceive(reply interface{}, err error, duration time.Duration) {
-	// If the Request Logging is configured to be skipped.
-	if !c.requestLoggerDecision {
+// req object wrt server.
+func (c *reporter) PostMsgReceive(req interface{}, err error, duration time.Duration) {
+	if !c.opts.shouldLog(interceptors.FullMethod(c.service, c.method), err) {
 		return
 	}
-	code := c.opts.codeFunc(err)
+	// If the first message is logged skip the rest of the logging.
+	if c.firstMessageLogged {
+		return
+	}
+	c.firstMessageLogged = true
+
 	logger := c.logger.With(extractFields(tags.Extract(c.ctx))...)
-	logRequestResponse(c.ctx, logger.With("grpc.recv.duration", duration.String()), reply, c.opts.levelFunc(code))
+	logger = logger.With("grpc.recv.duration", duration.String())
+	if err != nil {
+		logger = logger.With("error", fmt.Sprintf("%v", err))
+	}
+
+	if c.isServer {
+		logger.Log("logged the start of the request.", fmt.Sprintf("request object: %v", req))
+	} else {
+		logger.Log("logged the start of the response.", fmt.Sprintf("response object: %v", req))
+	}
 }
 
 type reportable struct {
@@ -83,30 +109,35 @@ type reportable struct {
 }
 
 func (r *reportable) ServerReporter(ctx context.Context, req interface{}, typ interceptors.GRPCType, service string, method string) (interceptors.Reporter, context.Context) {
-	reqLoggerDecider := r.requestLoggerDecision(ctx, interceptors.FullMethod(service, method), req)
-	return r.reporter(ctx, typ, service, method, KindServerFieldValue, reqLoggerDecider)
+	if !r.requestLoggerDecision(ctx, interceptors.FullMethod(service, method), req) {
+		return interceptors.NoopReporter{}, ctx
+	}
+	return r.reporter(ctx, typ, service, method, KindServerFieldValue, true)
 }
 
 func (r *reportable) ClientReporter(ctx context.Context, resp interface{}, typ interceptors.GRPCType, service string, method string) (interceptors.Reporter, context.Context) {
-	reqLoggerDecider := r.requestLoggerDecision(ctx, interceptors.FullMethod(service, method), resp)
-	return r.reporter(ctx, typ, service, method, KindClientFieldValue, reqLoggerDecider)
+	if !r.requestLoggerDecision(ctx, interceptors.FullMethod(service, method), resp) {
+		return interceptors.NoopReporter{}, ctx
+	}
+	return r.reporter(ctx, typ, service, method, KindClientFieldValue, false)
 }
 
-func (r *reportable) reporter(ctx context.Context, typ interceptors.GRPCType, service string, method string, kind string, requestLoggerDecision bool) (interceptors.Reporter, context.Context) {
+func (r *reportable) reporter(ctx context.Context, typ interceptors.GRPCType, service string, method string, kind string, isServer bool) (interceptors.Reporter, context.Context) {
 	fields := commonFields(kind, typ, service, method)
 	fields = append(fields, "grpc.start_time", time.Now().Format(time.RFC3339))
 	if d, ok := ctx.Deadline(); ok {
 		fields = append(fields, "grpc.request.deadline", d.Format(time.RFC3339))
 	}
 	return &reporter{
-		ctx:                   ctx,
-		typ:                   typ,
-		service:               service,
-		method:                method,
-		requestLoggerDecision: requestLoggerDecision,
-		opts:                  r.opts,
-		logger:                r.logger.With(fields...),
-		kind:                  kind,
+		ctx:                ctx,
+		typ:                typ,
+		service:            service,
+		method:             method,
+		firstMessageLogged: false,
+		isServer:           isServer,
+		opts:               r.opts,
+		logger:             r.logger.With(fields...),
+		kind:               kind,
 	}, ctx
 }
 
@@ -136,23 +167,4 @@ func UnaryServerInterceptor(logger Logger, decider PostRequestLoggingDecider, op
 func StreamServerInterceptor(logger Logger, decider PostRequestLoggingDecider, opts ...Option) grpc.StreamServerInterceptor {
 	o := evaluateServerOpt(opts)
 	return interceptors.StreamServerInterceptor(&reportable{logger: logger, requestLoggerDecision: decider, opts: o})
-}
-
-// TODO: yashrsharma44
-// What are the things that we want to log?
-func logRequestResponse(ctx context.Context, logger Logger, servObj interface{}, level Level) {
-	reqId, ok := RequestIDFromContext(ctx)
-	if ok {
-		logger = logger.With("request-id", reqId)
-	} else {
-		logger = logger.With("request-id", "NONE")
-	}
-	logger.Log(level, "logged details of the request")
-}
-
-// RequestIDFromContext returns the request id from context.
-func RequestIDFromContext(ctx context.Context) (string, bool) {
-	headers, ok := metadata.FromIncomingContext(ctx)
-	rid := headers["X-Request-ID"][0]
-	return rid, ok
 }
