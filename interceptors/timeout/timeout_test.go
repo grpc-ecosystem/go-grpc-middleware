68a56d23365b19b6dbe6907128661e21d90c8707
Anthony Moutte <instabledesign@gmail.com>
2020-09-03 11:43:10 +0200

    try increase timing

--- interceptors/timeout/timeout_test.go
+++ interceptors/timeout/timeout_test.go
@@ -18,16 +18,18 @@ type TimeoutTestServiceServer struct {
 }
 
 func (t *TimeoutTestServiceServer) PingEmpty(ctx context.Context, req *testpb.Empty) (*testpb.PingResponse, error) {
-	time.Sleep(t.sleepTime)
+	if t.sleepTime > 0 {
+		time.Sleep(t.sleepTime)
+	}
 	return t.TestPingService.PingEmpty(ctx, req)
 }
 
 func TestTimeoutUnaryClientInterceptor(t *testing.T) {
-	server := &TimeoutTestServiceServer{sleepTime: 1 * time.Millisecond}
+	server := &TimeoutTestServiceServer{}
 
 	its := &grpctesting.InterceptorTestSuite{
 		ClientOpts: []grpc.DialOption{
-			grpc.WithUnaryInterceptor(timeout.TimeoutUnaryClientInterceptor(10 * time.Millisecond)),
+			grpc.WithUnaryInterceptor(timeout.TimeoutUnaryClientInterceptor(100 * time.Millisecond)),
 		},
 		TestService: server,
 	}
@@ -35,22 +37,22 @@ func TestTimeoutUnaryClientInterceptor(t *testing.T) {
 	its.SetupSuite()
 	defer its.TearDownSuite()
 
-	// This call will take 1/10ms for respond, so the client timeout NOT exceed.
+	// This call will take 0/100ms for respond, so the client timeout NOT exceed.
 	resp, err := its.Client.PingEmpty(context.TODO(), &testpb.Empty{})
 	assert.NoError(t, err)
 	assert.NotNil(t, resp)
 	assert.Equal(t, "default_response_value", resp.Value)
 
-	// server will sleep 30ms before respond
-	server.sleepTime = 30 * time.Millisecond
+	// server will sleep 300ms before respond
+	server.sleepTime = 300 * time.Millisecond
 
-	// This call will take 30/10ms for respond, so the client timeout exceed.
+	// This call will take 300/100ms for respond, so the client timeout exceed.
 	resp2, err2 := its.Client.PingEmpty(context.TODO(), &testpb.Empty{})
 	assert.Nil(t, resp2)
 	assert.EqualError(t, err2, "rpc error: code = DeadlineExceeded desc = context deadline exceeded")
 
-	// This call will take 30/40ms for respond, so the client timeout NOT exceed.
-	longerValidityContext, cancel := context.WithTimeout(context.TODO(), 40*time.Millisecond)
+	// This call will take 300/400ms for respond, so the client timeout NOT exceed.
+	longerValidityContext, cancel := context.WithTimeout(context.TODO(), 400*time.Millisecond)
 	defer cancel()
 	resp3, err3 := its.Client.PingEmpty(longerValidityContext, &testpb.Empty{})
 	assert.NoError(t, err3)
