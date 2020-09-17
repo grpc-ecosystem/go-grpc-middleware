grpc-ecosystem/go-grpc-middleware

commit 99e4df761505c7e0d13a7e52f298dc4b3da94ff5

feature(tracing): Add datadog support to injectOpenTracingIdsToTags

tracing/opentracing/id_extract.go
@@ -23,6 +23,8 @@ const (
 // https://github.com/opentracing/basictracer-go/blob/1b32af207119a14b1b231d451df3ed04a72efebf/propagation_ot.go#L26
 // Jaeger from Uber use one-key schema with next format '{trace-id}:{span-id}:{parent-span-id}:{flags}'
 // https://www.jaegertracing.io/docs/client-libraries/#trace-span-identity
+// Datadog uses keys ending with 'trace-id' and 'parent-id' (for span) by default:
+// https://github.com/DataDog/dd-trace-go/blob/v1/ddtrace/tracer/textmap.go#L77
 func injectOpentracingIdsToTags(span opentracing.Span, tags grpc_ctxtags.Tags) {
 	if err := span.Tracer().Inject(span.Context(), opentracing.HTTPHeaders, &tagsCarrier{tags}); err != nil {
 		grpclog.Infof("grpc_opentracing: failed extracting trace info into ctx %v", err)
@@ -63,5 +65,13 @@ func (t *tagsCarrier) Set(key, val string) {
 				t.Tags.Set(TagSampled, "false")
 			}
 		}
+	}
+
+	if strings.HasSuffix(key, "trace-id") {
+		t.Tags.Set(TagTraceId, val)
+	}
+
+	if strings.HasSuffix(key, "parent-id") {
+		t.Tags.Set(TagSpanId, val)
 	}
 }

commit 80c2552d901465995f8cc0f8053b10b0307b8778

Fix the special case for jaeger format traceid extraction (#338)
* Fix the special case for jaeger format traceid extraction being overridden regression introduced with #262

* Add unit test for Jaeger trace id parsing fix

tracing/opentracing/id_extract.go
@@ -40,20 +40,6 @@ type tagsCarrier struct {
 
 func (t *tagsCarrier) Set(key, val string) {
 	key = strings.ToLower(key)
-	if strings.Contains(key, "traceid") {
-		t.Tags.Set(TagTraceId, val) // this will most likely be base-16 (hex) encoded
-	}
-
-	if strings.Contains(key, "spanid") && !strings.Contains(strings.ToLower(key), "parent") {
-		t.Tags.Set(TagSpanId, val) // this will most likely be base-16 (hex) encoded
-	}
-
-	if strings.Contains(key, "sampled") {
-		switch val {
-		case "true", "false":
-			t.Tags.Set(TagSampled, val)
-		}
-	}
 
 	if key == t.traceHeaderName {
 		parts := strings.Split(val, ":")
@@ -66,6 +52,23 @@ func (t *tagsCarrier) Set(key, val string) {
 			} else {
 				t.Tags.Set(TagSampled, "false")
 			}
+
+			return
+		}
+	}
+
+	if strings.Contains(key, "traceid") {
+		t.Tags.Set(TagTraceId, val) // this will most likely be base-16 (hex) encoded
+	}
+
+	if strings.Contains(key, "spanid") && !strings.Contains(strings.ToLower(key), "parent") {
+		t.Tags.Set(TagSpanId, val) // this will most likely be base-16 (hex) encoded
+	}
+
+	if strings.Contains(key, "sampled") {
+		switch val {
+		case "true", "false":
+			t.Tags.Set(TagSampled, val)
 		}
 	}
 

tracing/opentracing/id_extract_test.go
@@ -1,0 +1,32 @@
+package grpc_opentracing
+
+import (
+	"fmt"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTagsCarrier_Set_JaegerTraceFormat(t *testing.T) {
+	var (
+		fakeTraceSampled   = 1
+		fakeInboundTraceId = "deadbeef"
+		fakeInboundSpanId  = "c0decafe"
+		traceHeaderName    = "uber-trace-id"
+	)
+
+	traceHeaderValue := fmt.Sprintf("%s:%s:%s:%d", fakeInboundTraceId, fakeInboundSpanId, fakeInboundSpanId, fakeTraceSampled)
+
+	c := &tagsCarrier{
+		Tags:            grpc_ctxtags.NewTags(),
+		traceHeaderName: traceHeaderName,
+	}
+
+	c.Set(traceHeaderName, traceHeaderValue)
+
+	assert.EqualValues(t, map[string]interface{}{
+		TagTraceId: fakeInboundTraceId,
+		TagSpanId:  fakeInboundSpanId,
+		TagSampled: "true",
+	}, c.Tags.Values())
+}