f8489005087a94503ec229b6a66264a9bd8a61fe
Yash Sharma <yashrsharma44@gmail.com>
2021-01-29 19:18:42 +0530

    added a function for custom registered metrics object
    Signed-off-by: Yash Sharma <yashrsharma44@gmail.com>

--- providers/openmetrics/server_interceptor.go
+++ providers/openmetrics/server_interceptor.go
@@ -7,18 +7,17 @@ import (
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors"
 )
 
-var (
-	// DefaultServerMetrics is the default instance of ServerMetrics. It is
-	// intended to be used in conjunction the default Prometheus metrics
-	// registry.
-	DefaultServerMetrics = NewServerMetrics(openmetrics.DefaultRegisterer)
-)
+// RegisterServerMetrics returns a custom ServerMetrics object registered
+// with the user's registry, and registers some common metrics associated
+// with every instance.
+func RegisterServerMetrics(registry openmetrics.Registerer) *ServerMetrics {
+	customServerMetrics := NewServerMetrics(registry)
+	customServerMetrics.MustRegister(customServerMetrics.serverStartedCounter)
+	customServerMetrics.MustRegister(customServerMetrics.serverHandledCounter)
+	customServerMetrics.MustRegister(customServerMetrics.serverStreamMsgReceived)
+	customServerMetrics.MustRegister(customServerMetrics.serverStreamMsgSent)
 
-func init() {
-	DefaultServerMetrics.MustRegister(DefaultServerMetrics.serverStartedCounter)
-	DefaultServerMetrics.MustRegister(DefaultServerMetrics.serverHandledCounter)
-	DefaultServerMetrics.MustRegister(DefaultServerMetrics.serverStreamMsgReceived)
-	DefaultServerMetrics.MustRegister(DefaultServerMetrics.serverStreamMsgSent)
+	return customServerMetrics
 }
 
 // UnaryServerInterceptor is a gRPC server-side interceptor that provides Prometheus monitoring for Unary RPCs.
@@ -33,13 +32,4 @@ func StreamServerInterceptor(serverRegister openmetrics.Registerer) grpc.StreamS
 	return interceptors.StreamServerInterceptor(&reportable{
 		registry: serverRegister,
 	})
-}
-
-// RegisterAllMetrics takes a gRPC server and pre-initializes all counters to 0. This
-// allows for easier monitoring in Prometheus (no missing metrics), and should
-// be called *after* all services have been registered with the server. This
-// function acts on the DefaultServerMetrics variable.
-// If you are using a custom registry, use customServerMetrics.IntitializeMetrics() for the same.
-func RegisterAllMetrics(server *grpc.Server) {
-	DefaultServerMetrics.InitializeMetrics(server)
 }
