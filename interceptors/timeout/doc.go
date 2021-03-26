524ce8f5844d320bc6fd070eed5e9b2bb14a96f7
instabledesign <depotdesign@gmail.com>
2021-03-26 23:27:24 +0100

    v2: Client unary interceptor timeout on v2 branch (#330)
    * Client unary interceptor timeout on v2 branch
    
    * change fmt.println with log.Println in example (linter fix)
    
    * fix test
    
    * Update interceptors/timeout/examples_test.go
    
    Co-authored-by: Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>
    
    * fixup example linter
    
    * fix when context already has timeout
    
    * fixup trailing period
    
    * try increase timing
    
    * delete deadline check
    
    * update comment
    
    * add readme timeout note
    
    * fixup test
    
    Co-authored-by: Johan Brandhorst-Satzkorn <johan.brandhorst@gmail.com>

---
+++ interceptors/timeout/doc.go
@@ -0,0 +1,8 @@
+/*
+`grpc_timeout` are interceptors that timeout for gRPC client calls.
+
+Client Side Timeout Middleware
+
+Please see examples for simple examples of use.
+*/
+package timeout
---
+++ interceptors/timeout/examples_test.go
@@ -0,0 +1,35 @@
+package timeout_test
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/timeout"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/testing/testpb"
+	"google.golang.org/grpc"
+)
+
+// Initialization shows an initialization sequence with a custom client request timeout.
+func Example_initialization() {
+	clientConn, err := grpc.Dial(
+		"ServerAddr",
+		grpc.WithUnaryInterceptor(
+			// Set your client request timeout.
+			timeout.TimeoutUnaryClientInterceptor(20*time.Millisecond),
+		),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Initialize your grpc service with connection.
+	testServiceClient := testpb.NewTestServiceClient(clientConn)
+	resp, err := testServiceClient.Ping(context.TODO(), &testpb.PingRequest{Value: "my_example_value"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Use grpc response value.
+	log.Println(resp.Value)
+}
