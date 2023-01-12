71b9c36b0663163a707294e0f4584a653d40bd15
Michael Zhou <forsaken628@gmail.com>
2023-01-12 16:55:44 +0800

    remove settable

--- providers/zap/grpclogger.go
+++ providers/zap/grpclogger.go
@@ -6,7 +6,6 @@ package zap
 import (
 	"fmt"
 
-	grpc_logsettable "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging/settable"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/grpclog"
 )
@@ -15,42 +14,6 @@ var (
 	// SystemField is used in every log statement made through grpc_zap. Can be overwritten before any initialization code.
 	SystemField = zap.String("system", "grpc")
 )
-
-// ReplaceGrpcLogger sets the given zap.Logger as a gRPC-level logger.
-// This should be called *before* any other initialization, preferably from init() functions.
-// Deprecated: use ReplaceGrpcLoggerV2.
-func ReplaceGrpcLogger(logger *zap.Logger) {
-	zgl := &zapGrpcLogger{logger.With(SystemField, zap.Bool("grpc_log", true))}
-	grpclog.SetLogger(zgl)
-}
-
-type zapGrpcLogger struct {
-	logger *zap.Logger
-}
-
-func (l *zapGrpcLogger) Fatal(args ...interface{}) {
-	l.logger.Fatal(fmt.Sprint(args...))
-}
-
-func (l *zapGrpcLogger) Fatalf(format string, args ...interface{}) {
-	l.logger.Fatal(fmt.Sprintf(format, args...))
-}
-
-func (l *zapGrpcLogger) Fatalln(args ...interface{}) {
-	l.logger.Fatal(fmt.Sprint(args...))
-}
-
-func (l *zapGrpcLogger) Print(args ...interface{}) {
-	l.logger.Info(fmt.Sprint(args...))
-}
-
-func (l *zapGrpcLogger) Printf(format string, args ...interface{}) {
-	l.logger.Info(fmt.Sprintf(format, args...))
-}
-
-func (l *zapGrpcLogger) Println(args ...interface{}) {
-	l.logger.Info(fmt.Sprint(args...))
-}
 
 // ReplaceGrpcLoggerV2 replaces the grpclog.LoggerV2 with the provided logger.
 // It should be called before any gRPC functions. Logging verbosity defaults to info level.
@@ -68,22 +31,6 @@ func ReplaceGrpcLoggerV2WithVerbosity(logger *zap.Logger, verbosity int) {
 		verbosity: verbosity,
 	}
 	grpclog.SetLoggerV2(zgl)
-}
-
-// SetGrpcLoggerV2 replaces the grpc_log.Logger with the provided logger.
-// It can be used even when grpc infrastructure was initialized.
-func SetGrpcLoggerV2(settable grpc_logsettable.SettableLoggerV2, logger *zap.Logger) {
-	SetGrpcLoggerV2WithVerbosity(settable, logger, 0)
-}
-
-// SetGrpcLoggerV2WithVerbosity replaces the grpc_.LoggerV2 with the provided logger and verbosity.
-// It can be used even when grpc infrastructure was initialized.
-func SetGrpcLoggerV2WithVerbosity(settable grpc_logsettable.SettableLoggerV2, logger *zap.Logger, verbosity int) {
-	zgl := &zapGrpcLoggerV2{
-		logger:    logger.With(SystemField, zap.Bool("grpc_log", true)),
-		verbosity: verbosity,
-	}
-	settable.Set(zgl)
 }
 
 type zapGrpcLoggerV2 struct {
