5398fb72b80abf79a35c3bb2ec8c25e2f2c47be5
Rohan Raj <rohanraj7316@gmail.com>
2023-03-24 04:47:11 +0530

    fix: add copyright headers

--- interceptors/validator/options.go
+++ interceptors/validator/options.go
@@ -1,3 +1,6 @@
+// Copyright (c) The go-grpc-middleware Authors.
+// Licensed under the Apache License 2.0.
+
 package validator
 
 import "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
