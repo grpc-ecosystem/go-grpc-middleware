92310ae8850711a5b786024dd52c2a74bca78d6e
Vikas Kaushik <10323307+vkaushik@users.noreply.github.com>
2024-02-22 04:14:10 +0000

    back to original

--- examples/dummy.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) The go-grpc-middleware Authors.
-// Licensed under the Apache License 2.0.
-
-package examples
-
-// this file is added to fix CI
-// Makefile has is using $(MODULES) and commands like lint, fmt etc. are failing because no go file found in examples dir.
