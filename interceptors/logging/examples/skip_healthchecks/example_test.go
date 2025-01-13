ed865db82ac37402d67a9eb65ec8c1bb7da70b6f
Benjamin Kane <92759008+bkane-msft@users.noreply.github.com>
2025-01-13 08:05:40 -0800

    Add skip_healthchecks logging example (#742)
    Closes https://github.com/grpc-ecosystem/go-grpc-middleware/issues/741

---
+++ interceptors/logging/examples/skip_healthchecks/example_test.go
@@ -0,0 +1,67 @@
+// Copyright (c) The go-grpc-middleware Authors.
+// Licensed under the Apache License 2.0.
+
+package slog_test
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/selector"
+	"google.golang.org/grpc"
+)
+
+func InterceptorLogger() logging.Logger {
+	return logging.LoggerFunc(func(ctx context.Context, lvl logging.Level, msg string, fields ...any) {
+		fmt.Printf("This will not print anything for health checks or reflection, but will print this message for other requests")
+	})
+}
+
+func SkipHealthAndReflectionRequests(_ context.Context, c interceptors.CallMeta) bool {
+	return c.FullMethod() != "/grpc.health.v1.Health/Check" &&
+		c.FullMethod() != "/grpc.reflection.v1alpha.ServerReflection/ServerReflectionInfo"
+}
+
+func ExampleInterceptorLogger() {
+
+	opts := []logging.Option{
+		logging.WithLogOnEvents(logging.StartCall, logging.FinishCall),
+	}
+
+	// Create a server and wrap the logging interceptor with the selector interceptor to skip health checks and reflection requests.
+	_ = grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			// Wrap the logging interceptor with the selector interceptor to skip health checks and reflection requests.
+			selector.UnaryServerInterceptor(
+				logging.UnaryServerInterceptor(InterceptorLogger(), opts...),
+				selector.MatchFunc(SkipHealthAndReflectionRequests),
+			),
+		),
+		grpc.ChainStreamInterceptor(
+			selector.StreamServerInterceptor(
+				logging.StreamServerInterceptor(InterceptorLogger(), opts...),
+				selector.MatchFunc(SkipHealthAndReflectionRequests),
+			),
+		),
+	)
+
+	// Similarly you can create client and wrap the logging interceptor with the selector interceptor to skip health checks and reflection requests.
+	_, _ = grpc.Dial(
+		"some-target",
+		grpc.WithChainUnaryInterceptor(
+			selector.UnaryClientInterceptor(
+				logging.UnaryClientInterceptor(InterceptorLogger(), opts...),
+				selector.MatchFunc(SkipHealthAndReflectionRequests),
+			),
+		),
+		grpc.WithChainStreamInterceptor(
+			selector.StreamClientInterceptor(
+				logging.StreamClientInterceptor(InterceptorLogger(), opts...),
+				selector.MatchFunc(SkipHealthAndReflectionRequests),
+			),
+		),
+	)
+	// Output:
+}
